fba5ab17717ad4022715a0b6ac4f9c9dbd3ed333
Glen Langer <contao.modul@glen-langer.de>
2024-02-11 23:22:50 +0100

    #49, fix error strip_tags null given

--- src/Cron/StartJobsCron.php
+++ src/Cron/StartJobsCron.php
@@ -190,7 +190,7 @@ class StartJobsCron
                     $this->connection->executeStatement($sqlUpdate, $dataset, $types);
 
                     if ($cronJob['logging']) {
-                        if ('' !== $output) {
+                        if ('' !== $output || null !== $output) {
                             $this->logger?->error('Cron job '.$row['title'].': failed: '.strip_tags($output));
                         } else {
                             $this->logger?->info('Cron job '.$row['title'].': '.($cronJob['completed'] ? 'completed.' : 'processed partially.'));
